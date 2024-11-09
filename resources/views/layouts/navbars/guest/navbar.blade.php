ca074a01599b6ae6aa206eb8dda83ebb46e618d7
alexander01576 <alexandermendez01576@gmail.com>
2024-11-09 13:54:40 -0600

    Se modifica el login

--- resources/views/layouts/navbars/guest/navbar.blade.php
+++ resources/views/layouts/navbars/guest/navbar.blade.php
@@ -5,8 +5,8 @@
             <nav
                 class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0 mx-4">
                 <div class="container-fluid">
-                    <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="{{ route('home') }}">
-                        Argon Dashboard 2 Laravel
+                    <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3" href="{{ route('home') }}">
+                        Gestor escolar
                     </a>
                     <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
@@ -17,7 +17,7 @@
                             <span class="navbar-toggler-bar bar3"></span>
                         </span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navigation">
+                    {{-- <div class="collapse navbar-collapse" id="navigation">
                         <ul class="navbar-nav mx-auto">
                             <li class="nav-item">
                                 <a class="nav-link d-flex align-items-center me-2 active" aria-current="page"
@@ -45,7 +45,7 @@
                                     class="btn btn-sm mb-0 me-1 btn-primary">Free Download</a>
                             </li>
                         </ul>
-                    </div>
+                    </div> --}}
                 </div>
             </nav>
             <!-- End Navbar -->
